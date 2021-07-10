7adb272312cd436fef2eeeb1e25ced35149377cc
viljaa <viljaacode@gmail.com>
2021-07-10 18:01:54 +0300

    Changes to the argument evaluation structure

--- commands/Steam.ts
+++ commands/Steam.ts
@@ -8,43 +8,43 @@ export const command: Command = {
 	description: 'Commands related to Steam.',
 	execute(message: Message, args?: String[]) {
 		const apiKey = process.env.STEAM_API_KEY
-        
-        // Get steam account name with StemaID
-        if (args && args[0] === 'name') {
-            try {
-                const steamid = args[1]
-                getPlayerSummariesJSON(steamid, apiKey!)
-                .then(json => {
-                    const username = json.response.players[0].personaname
-                    message.channel.send(`SteamID ${steamid} belongs for a gentleman/gentlewoman named ${username}.`)
-                })
 
-            } catch (err) {
-                console.error(err)
-                message.channel.send('Could not process command, check your command and arguments.')
-            }
-        }
-        // Get total playtime on the account
-        if (args && args[0] === 'hours') {
-            try {
-                const steamid = args[1]
-
-                fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamid}&format=json`)
-                .then(res => res.json())
-                .then(json => {
-                    const gameCount = json.response.game_count
-                    const totalPlaytime = calculateTotalPlaytime(json)
+        if(args?.length) {
+            // Get steam account name with SteamID
+            if (args[0] === 'name') {
+                try {
+                    const steamid = args[1]
                     getPlayerSummariesJSON(steamid, apiKey!)
                     .then(json => {
                         const username = json.response.players[0].personaname
-                        message.channel.send(`${username} has total of ${totalPlaytime} hours played accross ${gameCount} different games.`)  
+                        message.channel.send(`SteamID ${steamid} belongs for a gentleman/gentlewoman named ${username}.`)
                     })
-                })
-            } catch (err) {
-                console.error(err)
-                message.channel.send('Could not process command, check your command and arguments.')
+                } catch (err) {
+                    console.error(err)
+                    message.channel.send('Could not process command, check your command and arguments.')
+                }
             }
+            // Get total playtime on the account
+            if (args[0] === 'hours') {
+                try {
+                    const steamid = args[1]
 
+                    fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamid}&format=json`)
+                    .then(res => res.json())
+                    .then(json => {
+                        const gameCount = json.response.game_count
+                        const totalPlaytime = calculateTotalPlaytime(json)
+                        getPlayerSummariesJSON(steamid, apiKey!)
+                        .then(json => {
+                            const username = json.response.players[0].personaname
+                            message.channel.send(`${username} has total of ${totalPlaytime} hours played accross ${gameCount} different games.`)  
+                        })
+                    })
+                } catch (err) {
+                    console.error(err)
+                    message.channel.send('Could not process command, check your command and arguments.')
+                }
+            }
         }
     }
 }
