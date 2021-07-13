ed56c9f804de4ff9f51abb6be05eab9a7108156c
viljaa <viljaacode@gmail.com>
2021-07-13 22:37:49 +0300

    GameNews additions, switch statement rework
    Switched (no pun intended) argument evaluation structure from
    if-statements to switch structure for clarity and performance gains.
    
    Added cron jobs for scraping news from IGN and RockPaperShotgun. Timed
    all scheduled hourly jobs to spread out evenly through out the hour in
    20 minute intervals.
    
    Created own function for generating the embedded news post from scraped
    data.

--- commands/GamingNews.ts
+++ commands/GamingNews.ts
@@ -13,24 +13,67 @@ export const command: Command = {
         /* Store the message in a variable, so that it can be used by cron jobs that use Discord.js methods. */
         commandMsg = message
         
-        /* Evaluate arguments for configuring the job. Command structure: !!gamingnews [action] [source] */
+        /* Evaluate arguments for configuring the job. Command structure: !!gamingnews [source] [action] */
 		if(args?.length){
-            /* Options for starting the news service. */
-            if(args[0] === 'activate') {
-                if(args[1] === 'pelaaja') {
-                    scrapePelaaja.start()
-                    message.channel.send('Roger that gamer, news from Pelaaja.fi incoming every hour!')
+            switch (args[0]) {
+                case 'pelaaja': {
+                    switch (args[1]) {
+                        case 'activate': {
+                            scrapePelaaja.start()
+                            message.channel.send('Roger that gamer, news from Pelaaja.fi incoming every hour!')
+                            break
+                        }
+                        case 'stop': {
+                            scrapePelaaja.stop()
+                            break
+                        }
+                    }
+                    break
+                }
+                case 'ign': {
+                    switch (args[1]) {
+                        case 'activate': {
+                            scrapeIgn.start()
+                            message.channel.send('Daring aren\'t we, news from IGN incoming every hour!')
+                            break
+                        }
+                        case 'stop': {
+                            scrapeIgn.stop()
+                            break
+                        }
+                    }
+                    break
+                }
+                case 'rps': {
+                    switch (args[1]) {
+                        case 'activate': {
+                            scrapeRPS.start()
+                            message.channel.send('I see you\'re a gamer of culture as well, news from RockPaperShotgun incoming every hour!')
+                            break
+                        }
+                        case 'stop': {
+                            scrapeRPS.stop()
+                            break
+                        }
+                    }
+                    break
                 }
             }
-            /* Options for stopping the news service. */
-            if(args[0] === 'stop') {
-                if(args[1] === 'pelaaja') {
-                    scrapePelaaja.stop()
-                }
-                message.channel.send(`The game news service for ${args[1]} is now off.`)
-            }
+            /* Confditional message for stopping the service, shared by all sources. */
+            if(args[1] === 'stop') message.channel.send(`The game news service for ${args[0]} is now off.`)
         }
-	},
+	}
+}
+
+/* Function for generating embedded news post message */
+function generateEmbeddedMessage(sourceName: string, scoop: string, articleUrl?: string, articleImgUrl?: string): MessageEmbed {
+    return new MessageEmbed()
+    .setColor('#7400b8')
+    .setTitle(`${sourceName}: ${scoop}`)
+    .setURL(articleUrl ? articleUrl: '')
+    .setAuthor('Vihtori-Bot News')
+    .setImage(articleImgUrl ? articleImgUrl : '')
+    .setTimestamp()
 }
 
 /* GameingNews cron-job configuration. */
@@ -49,21 +92,48 @@ const scrapePelaaja: CronJob = new CronJob('0 0 */1 * * *', () => {
         // Scrape text related data
         const textelement = $('.view-content>.views-row>.layout-title-box>.title-box-text>h4>a').first()
         const scoop = textelement.text()
-        const articleUrl = textelement.attr('href')
+        const articleUrl = `https://pelaaja.fi${textelement.attr('href')}`
 
         // Scarape image related data
         const articleImgUrl = $('.view-content>.views-row>.layout-title-box>.title-box-background>a>img').first().attr('src')
 
         // Create embedded message
-        const formattedMsg = new MessageEmbed()
-        .setColor('#7400b8')
-        .setTitle(`Pelaaja: ${scoop}`)
-        .setURL(`https://pelaaja.fi${articleUrl}`)
-        .setAuthor('Vihtori-Bot News')
-        .setImage(articleImgUrl!)
-        .setTimestamp()
+        commandMsg.channel.send(generateEmbeddedMessage('Pelaaja.fi', scoop, articleUrl, articleImgUrl))
+    })
+})
 
-        commandMsg.channel.send(formattedMsg)
-        console.log('Article from Pelaaja.fi fetched.')
+const scrapeIgn: CronJob = new CronJob('0 19 */1 * * *', () => {
+    fetch('https://nordic.ign.com/article/news')
+    .then(res => res.text())
+    .then(body => {
+        const $ = cheerio.load(body)
+
+        // Scrape text related data
+        const scoop = $('article>.m>h3>a').first().text()
+        const articleUrl = $('article>.m>h3>a').first().attr('href')
+
+        // Scrape image related data
+        const articleImgUrl = $('article>.t>a>img').first().attr('src')
+
+        //Create embedded message
+        commandMsg.channel.send(generateEmbeddedMessage('IGN', scoop, articleUrl, articleImgUrl))
+    })
+})
+
+const scrapeRPS: CronJob = new CronJob('0 39 */1 * * *', () => {
+    fetch('https://www.rockpapershotgun.com/news')
+    .then(res => res.text())
+    .then(body => {
+        const $ = cheerio.load(body)
+
+        // Scrape text related data
+        const scoop = $('.summary_list>li>article>a').first().attr('title')
+        const articleUrl = $('.summary_list>li>article>a').first().attr('href')
+
+        // Scrape image related data
+        const articleImgUrl = $('.summary_list>li>article>.thumbnail>img').first().attr('src')
+
+        //Create embedded message
+        commandMsg.channel.send(generateEmbeddedMessage('RockPaperShotgun', scoop!, articleUrl, articleImgUrl))
     })
 })
